d569a1f537b1c13027a02d20772216c288984b6b
Pettarn <1225076678@qq.com>
2020-02-19 22:09:34 +0800

    update

--- Promise A+/test.js
+++ Promise A+/test.js
@@ -1,11 +1,45 @@
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(args)
-    })
-}).then((data) => {
-    console.log(data)
-})
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
 
-function Promise (func) {
+function Promise (executer) {
+    let that = this
+    that.state = PENDING
+    that.value = null
+    that.error = null
+    that.fulfilledCallbacks = []
+    that.rejectedCallback = []
+
+    function resolve (val) {
+        if (value instanceof Promise) {
+            return value.then(resolve, reject)
+        }
+
+        setTimeout(() => {
+            that.state = FULFILLED
+            that.fulfilledCallbacks.forEach((callback) => callback(val))
+        })
+    }
     
+    try {
+        executer(resolve)
+    } catch (e) {
+        reject(e)
+    }
+} 
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    let that = this
+
+    if (that.state === PENDING) {
+        return new Promise((resolve, reject) => {
+            try {
+                that.fulfilledCallbacks.push((value) => {
+                    onFulfilled(value)
+                })
+            } catch (e) {
+                reject(e)
+            }
+        })
+    }
 }
