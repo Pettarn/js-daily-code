6d9ea22972c10197512563c31b1f79ef31644df2
Pettarn <1225076678@qq.com>
2020-02-23 20:55:41 +0800

    update .gitignore

--- test2.js
+++
@@ -1,6 +0,0 @@
-// console.log(JSON.stringify('string'))
-let arr = []
-console.log(typeof undefined)
-function func () {
-
-}
--- 数组扁平化/test.js
+++
@@ -1,7 +0,0 @@
-let arr = [
-    [1, 2],
-    3,
-    [4, 5, [6, [7]]]
-]
-
-console.log(arr.flat(3))
